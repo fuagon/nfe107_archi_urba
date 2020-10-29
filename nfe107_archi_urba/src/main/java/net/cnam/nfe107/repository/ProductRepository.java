d7f0bf1458d3b098bfeda8cb596c6279251636e8
Anthony Dagon <dagon.anthony@hotmail.com>
2020-10-29 10:47:59 +0100

    Add entity

---
+++ nfe107_archi_urba/src/main/java/net/cnam/nfe107/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package net.cnam.nfe107.repository;
+
+/*
+ * @created 29/10/2020/10/2020 - 10:40
+ * @project nfe107_archi_urba
+ * @author Ohtnaoh - AD
+*/
+    
+public class ProductRepository {
+}
