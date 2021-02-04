127e3450efc061daf28ccf98e845467fa7a21bb3
Anthony Dagon <dagon.anthony@hotmail.com>
2021-02-04 12:22:59 +0100

    try to patch cnamathon / address

--- address/src/main/java/net/cnam/nfe107/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package net.cnam.nfe107.repository;
-
-import net.cnam.nfe107.repository.model.AddressModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository <AddressModel, Long> {
-}
